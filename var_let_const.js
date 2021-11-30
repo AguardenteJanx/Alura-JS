3e4ab636d3b3e6ad09da658b3c4d494e2e13945b
Diana F. Pellin <diana.pellin@viasoft.com.br>
2021-11-30 16:56:23 -0300

    Aula 02 - Var, let e Const

---
+++ var_let_const.js
@@ -0,0 +1,38 @@
+//Var
+
+//var altura = 5;
+//var comprimento = 7;
+
+//area = altura * comprimento;
+//console.log(area); 
+//var area; 
+//Como no javaScript lê todo o código antes de executar, as variáveis podem ser declaradas após o seu usao. 
+//mas, por boa prática, deve-se declará-las antes de chamá-las. 
+
+//let forma = 'retângulo'; 
+//let altura = 5;
+//let comprimento = 7; 
+///let area;
+
+//if (forma === 'retângulo') {
+  //  area = altura * comprimento; 
+    
+//} else {
+//    area = (altura * comprimento) /2;
+//}
+
+//console.log(area);
+ 
+let area; 
+const forma = 'triangulo'; 
+const h = 5;
+const comprimento = 7; 
+
+if (forma === 'quadrado') {
+    area = h * comprimento; 
+    
+} else {
+   area = (h * comprimento) /2;
+}
+
+console.log(area); 
