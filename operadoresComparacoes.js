c45b3c85e951b64f46454729e6aa4af5f713b88b
Diana F. Pellin <diana.pellin@viasoft.com.br>
2021-12-03 17:08:26 -0300

    revisão dos operadores de comparação

---
+++ operadoresComparacoes.js
@@ -0,0 +1,18 @@
+// == -> comparação implícita
+const numero = 900;
+const texto = "900";
+
+console.log(numero == texto);
+console.log(numero === texto); //Retorna false, pois os três === validam tanto o valor quanto o tipo da var.
+
+//typeof -> é responsável por dizer qual o tipo da var
+ console.log(typeof numero);
+ console.log(typeof texto);
+
+// == só compara o valor
+// === compara o valor e o tipo de dados. 
+// As boas práticas dizem que devemos usar os três iguais e conersões explícitas
+
+//Conversão explíita
+numero();
+String();
