9f443527dccd07796bf38d01666038725a44f330
Diana F. Pellin <diana.pellin@viasoft.com.br>
2021-12-03 15:29:12 -0300

    Conversões de  string, number e boolean

---
+++ toString.js
@@ -0,0 +1,17 @@
+let telefone = 12341234;
+console.log('O telefone é ' + String(telefone)); //Aqui ocorre a conversão do número 12341234 para uma string “12341234"
+// e assim poderemos fazer a concatenação entre as strings
+
+console.log("O telefone é " + telefone.toString()); // o .toString() é uma outra forma para  fazer essa conversão,
+// que é mais parecida com outras linguagens de programação.
+
+let usuarioConectado= false;
+
+console.log (String(usuarioConectado)); // Aqui ocorre uma conversão do boolean para string
+usuarioConectado = true;
+console.log (String(usuarioConectado));
+
+//Cálculo com String
+let largura = '10';
+let altura = "5";
+console.log(Number(largura) * Number(altura)); // teremos a conversão de String para números, possibilitando a realização da da multiplicação
